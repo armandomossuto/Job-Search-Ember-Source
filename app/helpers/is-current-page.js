b880eedd8769cf0c398066f19d77369dca67502e
Armando Mossuto <armandomossuto@gmail.com>
2018-10-24 13:54:31 +0100

    a

--- app/helpers/is-current-page.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function isCurrentPage(params/*, hash*/) {
-  let [arg1, arg2] = params;
-  return arg1 === arg2;
-}
-
-export default helper(isCurrentPage);
